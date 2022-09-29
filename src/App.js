dffb53a309ac8af7c04e26da16f30f4981053d22
mjthedev <i@marrionjohnson.io>
2022-09-29 17:12:35 -0600

    created react app and todo.md

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="main">e-commerce-webapp</h1>
+    </div>
+  );
+}
+
+export default App;
