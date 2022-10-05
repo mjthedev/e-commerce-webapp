6dffd680b322bbc5104a2e3b2d5c644726a6b89d
mjthedev <i@marrionjohnson.io>
2022-10-05 10:12:12 -0600

    Modified index.js to properly introduce Router component

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import MainHome from './pages/navbarComponents/MainHome.jsx';
+import ErrorPage from './pages/Error/ErrorPage';
+import HolidayGifts from './pages/navbarComponents/HolidayGifts.js';
+import Sale from './pages/navbarComponents/Sale'
+import Women from './pages/navbarComponents/Women'
+import Men from './pages/navbarComponents/Men'
+import Kids from './pages/navbarComponents/Kids'
+import YoungAdults from './pages/navbarComponents/YoungAdults';
+import Activewear from './pages/navbarComponents/Activewear';
+import Home from './pages/navbarComponents/Home';
+import Beauty from './pages/navbarComponents/Beauty';
+import Designer from './pages/navbarComponents/Designer';
+import Gift from './pages/navbarComponents/Gift';
+import Threads from './pages/navbarComponents/Threads';
+
+
+
+
+
+function App() {
+  return (
+    <>
+          <Routes>
+            <Route path="/" element = {<MainHome/>}/>
+            <Route path="/HolidayGifts" element = {<HolidayGifts/>}/>
+            <Route path="/Sale" element = {<Sale/>}/>
+            <Route path="/Women" element = {<Women/>}/>
+            <Route path="/Men" element = {<Men/>}/>
+            <Route path="/Kids" element = {<Kids/>}/>
+            <Route path="/YoungAdults" element = {<YoungAdults/>}/>
+            <Route path="/Activewear" element = {<Activewear/>}/>
+            <Route path="/Home" element = {<Home/>}/>
+            <Route path="/Beauty" element = {<Beauty/>}/>
+            <Route path="/Designer" element = {<Designer/>}/>
+            <Route path="/Gift" element = {<Gift/>}/>
+            <Route path="/Threads" element = {<Threads/>}/>
+            <Route path="*" element={<ErrorPage/>}/>
+          </Routes>
+      </>      
+  );
+}
+
+export default App;
