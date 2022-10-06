9141b3e902baa5055547686e9bb8d114576663cc
mjthedev <i@marrionjohnson.io>
2022-10-06 09:26:54 -0600

    added footerComponents directory in components directory with corresponding files. added routes directory in components directory, also nested a footer directory in routes directory. Modified Error directory and corresponding ErrorPage.jsx file.

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const ErrorPage = () => {
   return (
-    <div>
-     <h1>ERROR! PAGE NOT FOUND</h1>
+    <div style={{padding:40, marginTop: 80}}>
+     <h1 style={{fontSize: 40}}>e-commerce</h1>
+     <br/>
+     <br/>
+     <br/>
+     <br/>
+     <p style={{fontWeight: 700, fontSize: 30}}>Our site hit a snag, but we're fixing it</p>
+     <br/>
+     <p>Please try again in a little while. In the meantime, you can call 1.888.888.8888</p>
     </div>
   )
 }
